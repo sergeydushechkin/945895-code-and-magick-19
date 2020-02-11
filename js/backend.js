1a4ba55653422760e839dd312fcbf67e0eae454d
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2020-02-11 21:17:10 +0300

    Добавил файл модуля для работы с сетью

---
+++ js/backend.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function () {
+
+  var URL_LOAD = 'https://js.dump.academy/code-and-magick/data';
+  var URL_SAVE = 'https://js.dump.academy/code-and-magick';
+
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+  };
+
+  var save = function (data, onLoad, onError) {
+
+  };
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+
+})();
