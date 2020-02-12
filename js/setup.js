c12e297a046f92e80094b5309f43d88ad6f4a095
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2020-02-12 20:47:07 +0300

    Сделал загрузку волшебников

--- js/setup.js
+++ js/setup.js
@@ -5,38 +5,41 @@
   var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var wizardsList = document.querySelector('.setup-similar-list');
 
-  // Создаем DOM-элементы с магов
+  // Создает DOM-элементы с магов
   var renderWizards = function (wizard) {
     var wizardModel = wizardTemplate.cloneNode(true);
-    wizardModel.querySelector('.setup-similar-label').textContent = wizard.fullname;
-    wizardModel.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardModel.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    // wizardModel.querySelector('.setup-similar-label').textContent = wizard.fullname;
+    wizardModel.querySelector('.setup-similar-label').textContent = wizard.name;
+    // wizardModel.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardModel.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    // wizardModel.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardModel.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardModel;
   };
 
-  // Наполняем элемент волшебниками
-  var fillWizardsElement = function (wizards) {
+  // Наполняет элемент волшебниками
+  var fillWizardsElement = function (wizards, amount) {
     var fragment = document.createDocumentFragment();
-    for (var index = 0; index < wizards.length; index++) {
+    for (var index = 0; index < amount; index++) {
       fragment.appendChild(renderWizards(wizards[index]));
     }
     return fragment;
   };
 
-  // Загружаем список волшебников
-  var loadWizardsSuccess = function (w) {
-    wizardsList.appendChild(fillWizardsElement(window.data.generateWizards(WIZARDS_AMOUNT)));
+  // Загружает список волшебников
+  var loadWizardsSuccess = function (wizards) {
+    wizardsList.appendChild(fillWizardsElement(wizards, WIZARDS_AMOUNT));
   };
 
-  // Загружаем список волшебников
-  var loadWizardsError = function () {
-
+  // В случае ошибки при загрузке
+  var loadWizardsError = function (errorText) {
+    console.log(errorText);
   };
 
   /* ---------------Основной код--------------- */
 
   // wizardsList.appendChild(fillWizardsElement(window.data.generateWizards(WIZARDS_AMOUNT)));
   window.backend.load(loadWizardsSuccess, loadWizardsError);
-  wizardsList.appendChild(fillWizardsElement(window.data.generateWizards(WIZARDS_AMOUNT)));
+
 
 })();
