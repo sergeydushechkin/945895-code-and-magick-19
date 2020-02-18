a2080ab313346c6941eb26dfd5f0dfcf5729c23e
Sergey <dushechkin_serge@mail.ru>
2020-02-18 10:41:51 +0300

    Убрал мусор, поменял названия функций, теперь appendChild делает функция renderWizards

--- js/setup.js
+++ js/setup.js
@@ -6,29 +6,26 @@
   var wizardsList = document.querySelector('.setup-similar-list');
 
   // Создает DOM-элементы с магов
-  var renderWizards = function (wizard) {
+  var fillWizardsElement = function (wizard) {
     var wizardModel = wizardTemplate.cloneNode(true);
-    // wizardModel.querySelector('.setup-similar-label').textContent = wizard.fullname;
     wizardModel.querySelector('.setup-similar-label').textContent = wizard.name;
-    // wizardModel.querySelector('.wizard-coat').style.fill = wizard.coatColor;
     wizardModel.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
-    // wizardModel.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
     wizardModel.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardModel;
   };
 
   // Наполняет элемент волшебниками
-  var fillWizardsElement = function (wizards, amount) {
+  var renderWizards = function (wizards, amount) {
     var fragment = document.createDocumentFragment();
     for (var index = 0; index < amount; index++) {
-      fragment.appendChild(renderWizards(wizards[index]));
+      fragment.appendChild(fillWizardsElement(wizards[index]));
     }
-    return fragment;
+    wizardsList.appendChild(fragment);
   };
 
   // Загружает список волшебников
   var loadWizardsSuccess = function (wizards) {
-    wizardsList.appendChild(fillWizardsElement(wizards, WIZARDS_AMOUNT));
+    renderWizards(wizards, WIZARDS_AMOUNT);
   };
 
   // В случае ошибки при загрузке
@@ -38,7 +35,6 @@
 
   /* ---------------Основной код--------------- */
 
-  // wizardsList.appendChild(fillWizardsElement(window.data.generateWizards(WIZARDS_AMOUNT)));
   window.backend.load(loadWizardsSuccess, loadWizardsError);
 
 
